9ecb52ae2fcf58330feb0e0e7060c455803dd836
Gabriel Raskov <gabriel@raskov.se>
2022-10-16 16:23:24 +0200

    work done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../../styles/header.scss'
+import { auth } from '../firebase'
 
 export default function Header() {
   const [sideMenuHide, setSideMenuHide] = useState(true)
 
+  const [username, setUsername] = useState()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser: any) => {
+        if(currentUser == undefined) {
+
+        } else {
+            setUsername(currentUser.email);
+        }
+      });
+
+      
+}, [])
+  
   return (
     <>
 
@@ -22,12 +38,21 @@ export default function Header() {
       </header>
 
       <div className={sideMenuHide ? "sidemenu hide" : "sidemenu"}>
+        <div className={"sidemenu-top"}>
         <div onClick={() => setSideMenuHide(!sideMenuHide)} className={"sidemenu-hamburger-container"}>
 
-          <div className={"header-hamburger-bun"}></div>
-          <div className={"header-hamburger-meat"}></div>
+        <div className={"header-hamburger-bun"}></div>
+            <div className={"header-hamburger-meat"}></div>
           <div className={"header-hamburger-bun"}></div>
 
+      </div>
+
+<h3 className={"username"}>{username}</h3>
+        </div>
+
+        <div>
+          <h3>Logout</h3>
+          <h3>Leaderbord</h3>
         </div>
 
       </div>
